935262931c10a2ba3f6fc783a9c84154babdc6db
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-05 18:21:52 +0600

    Create validator.py

---
+++ helper/validator.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+import re
+from requests import head
+from util.six import withMetaclass
+from util.singleton import Singleton
+from handler.configHandler import ConfigHandler
+
+conf = ConfigHandler()
+
+HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0',
+          'Accept': '*/*',
+          'Connection': 'keep-alive',
+          'Accept-Language': 'zh-CN,zh;q=0.8'}
+
+IP_REGEX = re.compile(r"(.*:.*@)?\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}")
+
+
+class ProxyValidator(withMetaclass(Singleton)):
+    pre_validator = []
+    http_validator = []
+    https_validator = []
+
+    @classmethod
+    def addPreValidator(cls, func):
+        cls.pre_validator.append(func)
+        return func
+
+    @classmethod
+    def addHttpValidator(cls, func):
+        cls.http_validator.append(func)
+        return func
+
+    @classmethod
+    def addHttpsValidator(cls, func):
+        cls.https_validator.append(func)
+        return func
+
+
+@ProxyValidator.addPreValidator
+def formatValidator(proxy):
+    """Check proxy format"""
+    return True if IP_REGEX.fullmatch(proxy) else False
+
+
+@ProxyValidator.addHttpValidator
+def httpTimeOutValidator(proxy):
+    """ http detection timeout """
+
+    proxies = {"http": "http://{proxy}".format(proxy=proxy), "https": "https://{proxy}".format(proxy=proxy)}
+
+    try:
+        r = head(conf.httpUrl, headers=HEADER, proxies=proxies, timeout=conf.verifyTimeout)
+        return True if r.status_code == 200 else False
+    except Exception as e:
+        return False
+
+
+@ProxyValidator.addHttpsValidator
+def httpsTimeOutValidator(proxy):
+    """https detection timeout"""
+
+    proxies = {"http": "http://{proxy}".format(proxy=proxy), "https": "https://{proxy}".format(proxy=proxy)}
+    try:
+        r = head(conf.httpsUrl, headers=HEADER, proxies=proxies, timeout=conf.verifyTimeout, verify=False)
+        return True if r.status_code == 200 else False
+    except Exception as e:
+        return False
+
+
+@ProxyValidator.addHttpValidator
+def customValidatorExample(proxy):
+    """Customize the validator function to verify whether the agent is available and return True/False"""
+    return True
